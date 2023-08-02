0297340806cc94c0a1567d270c4afd2330100023
SimranjitKaur665 <bhangusimran530@gmail.com>
2023-08-02 11:21:50 -0400

    Haigaaa

---
+++ WebApplication1/WebApplication1/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }    
+        public int Age { get; set; }
+        public string City { get; set;}
+
+        public Person(string name,int age,string city)
+        {
+            Name = name;
+            Age = age;
+            City = city;
+        }
+    }
+}
